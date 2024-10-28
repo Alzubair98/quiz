8101591f3356b65fc2be096c9995ba26b4e87422
Alzubair98 <203721133@ogr.altinbas.edu.tr>
2024-10-28 22:44:23 +0300

    add result comp to app comp

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="ctr">
     <questions></questions>
-    <div class="result">
-      <div class="title">You got sample result 1!</div>
-      <div class="desc">Enter a short description here about the result.</div>
-    </div>
+    <results></results>
+
     <button type="button" class="reset-btn">Reset</button>
   </div>
 </template>
 
 <script>
 import Questions from "./component/Questions.vue";
+import Results from "./component/Results.vue";
 export default {
   name: "App",
   components: {
     Questions,
+    Results,
   },
 };
 </script>
--- src/component/Results.vue
+++ src/component/Results.vue
@@ -1,5 +1,12 @@
-<template></template>
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">Enter a short description here about the result.</div>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  name: "results",
+};
 </script>
